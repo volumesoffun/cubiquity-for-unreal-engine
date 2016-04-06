c909d9e2fec3486d1cc543b40675e8ee7c36f023
xoyojank <xoyojank@gmail.com>
2016-04-06 18:55:31 +0800

    move example-vdb to /Content/ directory

--- Source/Cubiquity/Private/CubiquityTerrainVolume.cpp
+++ Source/Cubiquity/Private/CubiquityTerrainVolume.cpp
@@ -9,65 +9,65 @@
 #include "CubiquityMaterialSet.h"
 
 ACubiquityTerrainVolume::ACubiquityTerrainVolume(const FObjectInitializer& PCIP)
-	: Super(PCIP)
+    : Super(PCIP)
 {
-	volumeFileName = TEXT("f:/Documents/Unreal Projects/Cubiquity-UE4-Test/Cubiquity-Dependencies/example-vdb/SmoothVoxeliensTerrain.vdb");
+    volumeFileName = FPaths::GameContentDir() + "example-vdb/SmoothVoxeliensTerrain.vdb";
 }
 
 void ACubiquityTerrainVolume::PostActorCreated()
 {
-	UE_LOG(CubiquityLog, Log, TEXT("ACubiquityTerrainVolume::PostActorCreated"));
-	
-	Super::PostActorCreated();
+    UE_LOG(CubiquityLog, Log, TEXT("ACubiquityTerrainVolume::PostActorCreated"));
+
+    Super::PostActorCreated();
 }
 
 void ACubiquityTerrainVolume::PostLoad()
 {
-	UE_LOG(CubiquityLog, Log, TEXT("ACubiquityTerrainVolume::PostLoad"));
+    UE_LOG(CubiquityLog, Log, TEXT("ACubiquityTerrainVolume::PostLoad"));
 
-	Super::PostLoad();
+    Super::PostLoad();
 }
 
 void ACubiquityTerrainVolume::Destroyed()
 {
-	Super::Destroyed();
+    Super::Destroyed();
 
-	m_volume.reset(nullptr);
+    m_volume.reset(nullptr);
 }
 
 void ACubiquityTerrainVolume::loadVolume()
 {
-	m_volume = loadVolumeImpl<Cubiquity::TerrainVolume>();
+    m_volume = loadVolumeImpl<Cubiquity::TerrainVolume>();
 }
 
 void ACubiquityTerrainVolume::sculptTerrain(FVector localPosition, float innerRadius, float outerRadius, float opacity)
 {
-	m_volume->sculpt({ localPosition.X, localPosition.Y, localPosition.Z }, innerRadius, outerRadius, opacity);
+    m_volume->sculpt({ localPosition.X, localPosition.Y, localPosition.Z }, innerRadius, outerRadius, opacity);
 }
 
 FVector ACubiquityTerrainVolume::pickSurface(FVector localStartPosition, FVector localDirection) const
 {
-	bool success;
-	auto hitLocation = m_volume->pickSurface({ localStartPosition.X, localStartPosition.Y, localStartPosition.Z }, { localDirection.X, localDirection.Y, localDirection.Z }, &success);
+    bool success;
+    auto hitLocation = m_volume->pickSurface({ localStartPosition.X, localStartPosition.Y, localStartPosition.Z }, { localDirection.X, localDirection.Y, localDirection.Z }, &success);
 
-	if (!success)
-	{
-		UE_LOG(CubiquityLog, Log, TEXT("Surface pick found nothing"));
-		return FVector::ZeroVector;
-	}
+    if (!success)
+    {
+        UE_LOG(CubiquityLog, Log, TEXT("Surface pick found nothing"));
+        return FVector::ZeroVector;
+    }
 
-	return { hitLocation.x, hitLocation.y, hitLocation.z };
+    return { hitLocation.x, hitLocation.y, hitLocation.z };
 }
 
 void ACubiquityTerrainVolume::setVoxel(FVector position, const UCubiquityMaterialSet* materialSet)
 {
-	m_volume->setVoxel({ position.X, position.Y, position.Z }, *materialSet);
+    m_volume->setVoxel({ position.X, position.Y, position.Z }, *materialSet);
 }
 
 UCubiquityMaterialSet* ACubiquityTerrainVolume::getVoxel(FVector position) const
 {
-	const auto& voxel = m_volume->getVoxel({ position.X, position.Y, position.Z });
-	auto materialSet = NewObject<UCubiquityMaterialSet>();
-	materialSet->m_materialSet = voxel;
-	return materialSet;
+    const auto& voxel = m_volume->getVoxel({ position.X, position.Y, position.Z });
+    auto materialSet = NewObject<UCubiquityMaterialSet>();
+    materialSet->m_materialSet = voxel;
+    return materialSet;
 }
